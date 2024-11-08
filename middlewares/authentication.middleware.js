9030efc2c54186a40141752716989c10fc5f360c
saudkhanbpk <saudkhanbpk@gmail.com>
2024-11-08 09:49:07 +0500

    google auth implemented

--- middlewares/authentication.middleware.js
+++ middlewares/authentication.middleware.js
@@ -4,11 +4,6 @@ import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/user.model.js";
 
 export const isAuthenticated = asyncHandler(async (req, res, next) => {
-  if (req.user) {
-    console.log(req.user);
-    return next();
-  }
-
   try {
     const token =
       req.cookies?.accessToken ||
