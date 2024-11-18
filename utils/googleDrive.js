b5771884a7c17bb4b26efa709d7ecd12139c9414
saudkhanbpk <saudkhanbpk@gmail.com>
2024-11-18 11:58:19 +0500

    getMyFolderForUploadContentToMyOrder and createFolderAndUploadMultipleFiles

--- utils/googleDrive.js
+++ utils/googleDrive.js
@@ -17,9 +17,10 @@ const auth = new google.auth.GoogleAuth({
 const drive = google.drive({ version: "v3", auth });
 
 /**
- * Get MIME type for a file based on its extension
- * @param {string} filePath - Path to the file
- * @returns {string} MIME type
+ * Returns the mime type for the given file path based on its extension.
+ *
+ * @param {string} filePath - The path of the file to get the mime type for.
+ * @returns {string} The mime type for the given file path.
  */
 const getMimeType = (filePath) => {
   const extension = path.extname(filePath).toLowerCase();
@@ -56,66 +57,96 @@ const getMimeType = (filePath) => {
 };
 
 /**
- * Creates a folder in Google Drive and uploads a file to it
- * @param {string} folderName - Name of the folder to create
- * @param {string} filePath - Path to the file to upload
- * @returns {Promise<string>} The ID of the uploaded file
- * @throws {ApiError} If folder creation or file upload fails
+ * Retrieves all files in the given folder, excluding trashed files.
+ *
+ * @param {string} folderId - The ID of the folder to retrieve files from.
+ * @returns {Promise<File[]>} A promise that resolves with an array of file objects.
  */
-const createFolderAndFile = async (folderName, filePath) => {
-  if (!process.env.GOOGLE_DRIVE_FOLDER_ID) {
-    throw new ApiError(500, "Google Drive Root folder ID not configured.");
-  }
-
-  const folderMetadata = {
-    name: folderName,
-    mimeType: "application/vnd.google-apps.folder",
-    parents: [process.env.GOOGLE_DRIVE_FOLDER_ID],
-  };
-
+const listAllFilesInFolder = async (folderId) => {
   try {
-    const folder = await drive.files.create({
-      resource: folderMetadata,
-      fields: "id",
+    const response = await drive.files.list({
+      q: `'${folderId}' in parents and trashed = false`,
+      fields: "files(id, name, mimeType, size, createdTime, modifiedTime)",
+      spaces: "drive",
     });
 
-    if (!folder.data.id) {
-      throw new ApiError(500, "Folder creation failed: No folder ID returned.");
-    }
-
-    const fileMetadata = {
-      name: path.basename(filePath),
-      parents: [folder.data.id],
-    };
-
-    const file = await drive.files.create({
-      resource: fileMetadata,
-      media: {
-        mimeType: getMimeType(filePath),
-        body: fs.createReadStream(filePath),
-      },
-      fields: "id, mimeType",
-    });
-
-    if (!file.data.id) {
-      throw new ApiError(500, "File upload failed: No file ID returned.");
-    }
-
-    return file.data.id;
+    return response.data.files;
   } catch (error) {
-    throw new ApiError(
-      500,
-      `Folder creation or file upload error: ${error.message}`
-    );
+    throw new ApiError(500, `File retrieval error: ${error.message}`);
   }
 };
 
 /**
- * Create a folder in Google Drive.
+ * Retrieves all folders in the given folder, excluding trashed folders.
+ *
+ * @param {string} [folderId] - The ID of the folder to retrieve folders from.
+ *                              If not provided, it defaults to the value set
+ *                              in the GOOGLE_DRIVE_FOLDER_ID environment variable.
+ * @returns {Promise<File[]>} A promise that resolves with an array of folder objects.
+ */
+const listAllFoldersInFolder = async (
+  folderId = process.env.GOOGLE_DRIVE_FOLDER_ID
+) => {
+  try {
+    const response = await drive.files.list({
+      q: `mimeType = 'application/vnd.google-apps.folder' and '${folderId}' in parents`,
+      fields: "files(id,name,mimeType,createdTime,modifiedTime)",
+      spaces: "drive",
+    });
+
+    return response.data.files;
+  } catch (error) {
+    throw new ApiError(500, `Folder listing error: ${error.message}`);
+  }
+};
+
+/**
+ * Retrieves a specific folder from the given parent folder, excluding trashed folders.
+ *
+ * @param {string} parentFolderId - The ID of the parent folder to retrieve the specific folder from.
+ * @param {string} specificFolderId - The ID of the specific folder to retrieve.
+ * @returns {Promise<File>} A promise that resolves with the specific folder object. If the folder is not found, it throws an ApiError with a 404 status.
+ */
+const listSpecificFolderInParentFolder = async (
+  /*************  ✨ Codeium Command ⭐  *************/
+  /**
+   * Retrieves a specific folder from the given parent folder, excluding trashed folders.
+   *
+   * @param {string} parentFolderId - The ID of the parent folder to retrieve the specific folder from.
+   * @param {string} specificFolderId - The ID of the specific folder to retrieve.
+   * @returns {Promise<File>} A promise that resolves with the specific folder object. If the folder is not found, it throws an ApiError with a 404 status.
+   */
+  /******  569eba65-9cb6-4e8f-a480-8a29ebb944ca  *******/ parentFolderId,
+  specificFolderId
+) => {
+  try {
+    const response = await drive.files.list({
+      q: `'${parentFolderId}' in parents and trashed = false`,
+      fields: "files(id, name, mimeType, size, createdTime, modifiedTime)",
+      spaces: "drive",
+    });
+
+    const files = response.data.files;
+
+    const specificFolder = files.find((file) => file.id === specificFolderId);
+
+    if (!specificFolder) {
+      throw new ApiError(404, "Specific folder not found in parent folder");
+    }
+
+    return specificFolder;
+  } catch (error) {
+    console.error("Google Drive API error:", error);
+    throw new ApiError(500, `File retrieval error: ${error.message}`);
+  }
+};
+
+/**
+ * Creates a folder in Google Drive.
+ *
  * @param {string} folderName - The name of the folder to create.
- * @param {string} [parentFolderId] - Optional ID of the parent folder.
- * @returns {Promise<string>} - The ID of the created folder.
- * @throws {Error} If folder creation fails.
+ * @param {string} [parentFolderId] - The ID of the parent folder to create the folder in. Defaults to the environment variable GOOGLE_DRIVE_FOLDER_ID.
+ * @returns {Promise<string>} A promise that resolves with the ID of the created folder. If the folder creation fails, it throws an Error with a message describing the failure.
  */
 const createFolder = async (
   folderName,
@@ -145,11 +176,76 @@ const createFolder = async (
 };
 
 /**
- * Uploads multiple files to a specified Google Drive folder
- * @param {string} folderId - ID of the target folder
- * @param {string[]} filePaths - Array of file paths to upload
- * @returns {Promise<Array>} Array of uploaded file metadata
- * @throws {ApiError} If file upload fails
+ * Creates a folder in Google Drive, uploads multiple files to it, and returns the IDs of the uploaded files.
+ *
+ * @param {string} folderName - The name of the folder to create.
+ * @param {Array<string>} filePaths - The list of local file paths to upload to the created folder.
+ * @returns {Promise<Array<string>>} A promise that resolves with an array of file IDs. If the folder creation or file upload fails, it throws an Error with a message describing the failure.
+ */
+const createFolderAndUploadMultipleFiles = async (folderName, filePaths) => {
+  const folderMetadata = {
+    name: folderName,
+    mimeType: "application/vnd.google-apps.folder",
+    parents: [process.env.GOOGLE_DRIVE_FOLDER_ID],
+  };
+
+  try {
+    // Create the folder
+    const folder = await drive.files.create({
+      resource: folderMetadata,
+      fields: "id",
+    });
+
+    if (!folder.data.id) {
+      throw new ApiError(500, "Folder creation failed: No folder ID returned.");
+    }
+
+    // Prepare an array to hold the results of each file upload
+    const fileUploadPromises = filePaths.map(async (filePath) => {
+      const fileMetadata = {
+        name: path.basename(filePath),
+        parents: [folder.data.id],
+      };
+
+      // Upload the file
+      const file = await drive.files.create({
+        resource: fileMetadata,
+        media: {
+          mimeType: getMimeType(filePath),
+          body: fs.createReadStream(filePath),
+        },
+        fields: "id, mimeType",
+      });
+
+      if (!file.data.id) {
+        throw new ApiError(
+          500,
+          `File upload failed for ${filePath}: No file ID returned.`
+        );
+      }
+
+      return file.data.id; // Return the file ID
+    });
+
+    // Wait for all file uploads to complete
+    const fileIds = await Promise.all(fileUploadPromises);
+
+    return fileIds; // Return all the file IDs
+  } catch (error) {
+    throw new ApiError(
+      500,
+      `Folder creation or file upload error: ${error.message}`
+    );
+  }
+};
+
+/**
+ * Uploads multiple files to a specified Google Drive folder.
+ *
+ * @param {string} folderId - The ID of the Google Drive folder where the files will be uploaded.
+ * @param {Array<string>} filePaths - An array of local file paths to the files that need to be uploaded.
+ * @returns {Promise<Array<object>>} A promise that resolves with an array of uploaded file data objects.
+ * @throws {ApiError} Throws an ApiError if any file upload operation fails.
  */
 const uploadFilesToFolder = async (folderId, filePaths) => {
   try {
@@ -178,16 +274,17 @@ const uploadFilesToFolder = async (folderId, filePaths) => {
 };
 
 /**
- * Finds a folder ID by its name within the configured parent folder
- * @param {string} folderName - Name of the folder to find
- * @returns {Promise<string|null>} Folder ID if found, null otherwise
- * @throws {ApiError} If the search operation fails
+ * Searches for a Google Drive folder by name in the top-level folder specified
+ * by the GOOGLE_DRIVE_FOLDER_ID environment variable, and returns the ID of
+ * the folder if it exists, or null if it does not.
+ *
+ * @param {string} folderName - The name of the folder to search for.
+ * @returns {Promise<string|null>} A promise that resolves with the ID of the
+ * folder if it exists, or null if it does not.
+ * @throws {ApiError} Throws an ApiError if there is an error while searching
+ * for the folder.
  */
 const getFolderIdByName = async (folderName) => {
-  if (!process.env.GOOGLE_DRIVE_FOLDER_ID) {
-    throw new ApiError(500, "Google Drive Root folder ID not configured.");
-  }
-
   try {
     const response = await drive.files.list({
       q: `name='${folderName}' and mimeType='application/vnd.google-apps.folder' and '${process.env.GOOGLE_DRIVE_FOLDER_ID}' in parents`,
@@ -202,29 +299,17 @@ const getFolderIdByName = async (folderName) => {
 };
 
 /**
- * Deletes a folder and all its contents from Google Drive
- * @param {string} folderId - ID of the folder to delete
- * @throws {ApiError} If deletion fails
- */
-const deleteFolder = async (folderId) => {
-  try {
-    const files = await listAllFilesInFolder(folderId);
-    const deletePromises = files.map((file) =>
-      drive.files.delete({ fileId: file.id })
-    );
-    await Promise.all(deletePromises);
-
-    await drive.files.delete({ fileId: folderId });
-  } catch (error) {
-    throw new ApiError(500, `Folder deletion error: ${error.message}`);
-  }
-};
-
-/**
- * Retrieves download URLs for all files in a folder
- * @param {string} folderId - ID of the folder
- * @returns {Promise<Array>} Array of objects containing file information and download URLs
- * @throws {ApiError} If retrieving URLs fails
+ * Retrieves an array of file objects from a Google Drive folder, with the
+ * "url" property set to a publicly accessible download URL for each file.
+ *
+ * @param {string} folderId - The ID of the Google Drive folder to retrieve
+ * files from.
+ * @returns {Promise<Array<object>>} A promise that resolves with an array of
+ * objects containing information about the files in the folder, including
+ * the file ID, name, MIME type, size, created time, modified time, and a
+ * publicly accessible download URL.
+ * @throws {ApiError} Throws an ApiError if there is an error while retrieving
+ * the file URLs.
  */
 const getFileUrlsFromFolder = async (folderId) => {
   try {
@@ -244,69 +329,34 @@ const getFileUrlsFromFolder = async (folderId) => {
 };
 
 /**
- * Lists all files in a specified Google Drive folder
- * @param {string} folderId - ID of the folder
- * @returns {Promise<Array>} Array of file metadata
- * @throws {ApiError} If retrieval fails
+ * Deletes a Google Drive folder and all its contents.
+ *
+ * @param {string} folderId - The ID of the folder to delete.
+ * @throws {ApiError} Throws an ApiError if there is an error while deleting
+ * the folder or its contents.
  */
-const listAllFilesInFolder = async (folderId) => {
+const deleteFolder = async (folderId) => {
   try {
-    const response = await drive.files.list({
-      q: `'${folderId}' in parents and trashed = false`,
-      fields: "files(id, name, mimeType, size, createdTime, modifiedTime)",
-      spaces: "drive",
-    });
+    const files = await listAllFilesInFolder(folderId);
+    const deletePromises = files.map((file) =>
+      drive.files.delete({ fileId: file.id })
+    );
+    await Promise.all(deletePromises);
 
-    return response.data.files;
+    await drive.files.delete({ fileId: folderId });
   } catch (error) {
-    throw new ApiError(500, `File retrieval error: ${error.message}`);
-  }
-};
-
-/**
- * Lists all folders within the configured parent folder
- * @returns {Promise<Array>} Array of folder metadata
- * @throws {ApiError} If retrieval fails
- */
-const listAllFolders = async () => {
-  try {
-    const response = await drive.files.list({
-      q: `mimeType = 'application/vnd.google-apps.folder' and '${process.env.GOOGLE_DRIVE_FOLDER_ID}' in parents`,
-      fields: "files(id,name,mimeType,createdTime,modifiedTime)",
-      spaces: "drive",
-    });
-
-    return response.data.files;
-  } catch (error) {
-    throw new ApiError(500, `Folder listing error: ${error.message}`);
-  }
-};
-
-const listSpecificFolderInParentFolder = async (
-  parentFolderId,
-  specificFolderId
-) => {
-  try {
-    const response = await drive.files.list({
-      q: `'${parentFolderId}' in parents and '${specificFolderId}' in parents and trashed = false`,
-      fields: "files(id, name, mimeType, size, createdTime, modifiedTime)",
-      spaces: "drive",
-    });
-
-    return response.data.files;
-  } catch (error) {
-    throw new ApiError(500, `File retrieval error: ${error.message}`);
+    throw new ApiError(500, `Folder deletion error: ${error.message}`);
   }
 };
 
 export {
-  createFolderAndFile,
+  listAllFilesInFolder,
+  listAllFoldersInFolder,
+  listSpecificFolderInParentFolder,
   createFolder,
+  createFolderAndUploadMultipleFiles,
   uploadFilesToFolder,
   getFolderIdByName,
-  deleteFolder,
   getFileUrlsFromFolder,
-  listAllFilesInFolder,
-  listAllFolders,
-  listSpecificFolderInParentFolder,
+  deleteFolder,
 };
