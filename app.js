d64432ca1dbf632dfff8031a3d923fcea2d3b261
saudkhanbpk <saudkhanbpk@gmail.com>
2024-11-26 15:33:08 +0500

    additional service api issue soleved

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: "*",
   methods: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
   allowedHeaders: "X-Requested-With, Content-Type, Authorization",
   credentials: true,
