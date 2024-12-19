5b6179bfacc26685bbd2f0f4e930be73d4c91c5e
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-19 11:06:32 +0500

    brands images

--- routes/brand.routes.js
+++ routes/brand.routes.js
@@ -2,11 +2,11 @@ import express from "express";
 
 import { isAuthenticated } from "../middlewares/authentication.middleware.js";
 import {
-  createBrand,
-  deleteBrand,
-  getBrands,
-  getSingleBrand,
-  updateBrand,
+    createBrand,
+    deleteBrand,
+    getBrands,
+    getSingleBrand,
+    updateBrand,
 } from "../controllers/brand.controller.js";
 
 const router = express.Router();
@@ -16,5 +16,11 @@ router.get("/", isAuthenticated, getBrands);
 router.get("/:brandId", isAuthenticated, getSingleBrand);
 router.patch("/:brandId", isAuthenticated, updateBrand);
 router.delete("/:brandId", isAuthenticated, deleteBrand);
+router.patch(
+    "/change-brand-image/:brandId",
+    isAuthenticated,
+    uploadOnMulter.single("brandImage"),
+    updateBrand
+);
 
 export default router;
