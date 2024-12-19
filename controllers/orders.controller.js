9c484da5d495c94450bf21c728efc1d494b8741b
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-19 14:20:50 +0500

    brands images

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -4,178 +4,187 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 import { isValidId } from "../utils/commonHelpers.js";
+import BrandModel from "../models/brand.model.js";
 
 const createOrder = asyncHandler(async (req, res, next) => {
-  const {
-    noOfUgc,
-    totalPrice,
-    orderStatus,
-    paymentStatus,
-    contentsDelivered,
-    additionalServices,
-    preferences,
-    briefContent,
-    orderQuota,
-    numberOfRequests,
-  } = req.body;
+    const {
+        noOfUgc,
+        totalPrice,
+        orderStatus,
+        paymentStatus,
+        contentsDelivered,
+        additionalServices,
+        preferences,
+        briefContent,
+        orderQuota,
+        numberOfRequests,
+    } = req.body;
 
-  if (
-    !additionalServices ||
-    !additionalServices.platform ||
-    !additionalServices.duration ||
-    !additionalServices.edit ||
-    !additionalServices.aspectRatio
-  ) {
-    throw new ApiError(400, "Please provide all additional services");
-  }
+    if (
+        !additionalServices ||
+        !additionalServices.platform ||
+        !additionalServices.duration ||
+        !additionalServices.edit ||
+        !additionalServices.aspectRatio
+    ) {
+        throw new ApiError(400, "Please provide all additional services");
+    }
 
-  if (
-    !briefContent ||
-    !briefContent.brandName ||
-    !briefContent.brief ||
-    !briefContent.productServiceName ||
-    !briefContent.productServiceDesc
-  ) {
-    throw new ApiError(400, "Please provide all brief content");
-  }
+    if (
+        !briefContent ||
+        !briefContent.brandName ||
+        !briefContent.brief ||
+        !briefContent.productServiceName ||
+        !briefContent.productServiceDesc
+    ) {
+        throw new ApiError(400, "Please provide all brief content");
+    }
 
-  const newOrder = await Orders.create({
-    orderOwner: req.user._id,
-    noOfUgc,
-    totalPrice,
-    orderStatus,
-    paymentStatus,
-    contentsDelivered,
-    additionalServices,
-    preferences,
-    briefContent,
-    orderQuota,
-    numberOfRequests,
-  });
+    briefContent.brandName = briefContent.brandName.trim();
+    const brand = await BrandModel.findOne({
+        brandName: briefContent.brandName,
+    });
+    if (!brand) {
+        throw new ApiError(400, "Brand not found");
+    }
 
-  return res
-    .status(201)
-    .json(new ApiResponse(201, newOrder, "Order created successfully"));
+    const newOrder = await Orders.create({
+        orderOwner: req.user._id,
+        noOfUgc,
+        totalPrice,
+        orderStatus,
+        paymentStatus,
+        contentsDelivered,
+        additionalServices,
+        preferences,
+        briefContent: { ...briefContent, brandName: brand.brandName },
+        orderQuota,
+        numberOfRequests,
+    });
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201, newOrder, "Order created successfully"));
 });
 
 const getOrders = asyncHandler(async (req, res) => {
-  const orders = await Orders.find({ orderOwner: req.user._id });
+    const orders = await Orders.find({ orderOwner: req.user._id });
 
-  if (!orders) {
-    throw new ApiError(404, "No orders found");
-  }
+    if (!orders) {
+        throw new ApiError(404, "No orders found");
+    }
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, orders, "My Orders retrieved successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, orders, "My Orders retrieved successfully"));
 });
 
 const getOrder = asyncHandler(async (req, res) => {
-  const { orderId } = req.params;
+    const { orderId } = req.params;
 
-  const order = await Orders.findById(orderId);
+    const order = await Orders.findById(orderId);
 
-  if (!order) {
-    throw new ApiError(404, "Order not found");
-  }
+    if (!order) {
+        throw new ApiError(404, "Order not found");
+    }
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, order, "Order retrieved successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, order, "Order retrieved successfully"));
 });
 
 const updateOrder = asyncHandler(async (req, res) => {
-  const { orderId } = req.params;
-  const {
-    noOfUgc,
-    orderOwner,
-    orderStatus,
-    paymentStatus,
-    contentsDelivered,
-    additionalServices,
-    preferences,
-    briefContent,
-    orderQuota,
-    numberOfRequests,
-    uploadFiles,
-  } = req.body;
+    const { orderId } = req.params;
+    const {
+        noOfUgc,
+        orderOwner,
+        orderStatus,
+        paymentStatus,
+        contentsDelivered,
+        additionalServices,
+        preferences,
+        briefContent,
+        orderQuota,
+        numberOfRequests,
+        uploadFiles,
+    } = req.body;
 
-  const order = await Orders.findByIdAndUpdate(
-    orderId,
-    {
-      noOfUgc,
-      orderOwner,
-      orderStatus,
-      paymentStatus,
-      contentsDelivered,
-      additionalServices,
-      preferences,
-      briefContent,
-      orderQuota,
-      numberOfRequests,
-      uploadFiles,
-    },
-    { new: true }
-  );
+    const order = await Orders.findByIdAndUpdate(
+        orderId,
+        {
+            noOfUgc,
+            orderOwner,
+            orderStatus,
+            paymentStatus,
+            contentsDelivered,
+            additionalServices,
+            preferences,
+            briefContent,
+            orderQuota,
+            numberOfRequests,
+            uploadFiles,
+        },
+        { new: true }
+    );
 
-  if (!order) {
-    throw new ApiError(404, "Order not updated or not found");
-  }
+    if (!order) {
+        throw new ApiError(404, "Order not updated or not found");
+    }
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, order, "Order updated successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, order, "Order updated successfully"));
 });
 
 const deleteOrder = asyncHandler(async (req, res) => {
-  const { orderId } = req.params;
+    const { orderId } = req.params;
 
-  const order = await Orders.findByIdAndDelete(orderId);
+    const order = await Orders.findByIdAndDelete(orderId);
 
-  if (!order) {
-    throw new ApiError(404, "Order not found or not deleted");
-  }
+    if (!order) {
+        throw new ApiError(404, "Order not found or not deleted");
+    }
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, order, "Order deleted successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, order, "Order deleted successfully"));
 });
 
 const createClaimOnOrder = asyncHandler(async (req, res) => {
-  const { orderId } = req.params;
-  const { claimContent } = req.body;
+    const { orderId } = req.params;
+    const { claimContent } = req.body;
 
-  isValidId(orderId);
+    isValidId(orderId);
 
-  if (!claimContent) {
-    throw new ApiError(400, "Please provide claim content");
-  }
+    if (!claimContent) {
+        throw new ApiError(400, "Please provide claim content");
+    }
 
-  const order = await Orders.findById(orderId);
+    const order = await Orders.findById(orderId);
 
-  if (!order) {
-    throw new ApiError(404, "Order not found");
-  }
+    if (!order) {
+        throw new ApiError(404, "Order not found");
+    }
 
-  const claim = await Claims.create({
-    customer: req.user._id,
-    order: orderId,
-    claimContent,
-  });
+    const claim = await Claims.create({
+        customer: req.user._id,
+        order: orderId,
+        claimContent,
+    });
 
-  order.orderStatus = "revision";
-  await order.save();
+    order.orderStatus = "revision";
+    await order.save();
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, claim, "Order retrieved successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, claim, "Order retrieved successfully"));
 });
 
 export {
-  createOrder,
-  updateOrder,
-  deleteOrder,
-  getOrder,
-  getOrders,
-  createClaimOnOrder,
+    createOrder,
+    updateOrder,
+    deleteOrder,
+    getOrder,
+    getOrders,
+    createClaimOnOrder,
 };
