8942aa1ad78c0765ff3a0de620b421e9248a9896
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-25 14:46:05 +0500

    created custom pacakges routes with some extra stuff

--- routes/admin/adminCustomPackage.routes.js
+++ routes/admin/adminCustomPackage.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {
-  isAuthenticated,
-  isAdmin,
+    isAuthenticated,
+    isAdmin,
 } from "../../middlewares/authentication.middleware.js";
 import {
-  createCustomPackage,
-  deleteCustomPackage,
-  getAllCustomPackages,
-  getCustomPackageById,
-  updateCustomPackage,
+    createCustomPackage,
+    deleteCustomPackage,
+    getAllCustomPackages,
+    getAllCustomPackagesByCustomer,
+    getCustomPackageById,
+    updateCustomPackage,
 } from "../../controllers/admin/adminCustomPackage.controller.js";
 
 const router = express.Router();
@@ -19,6 +20,7 @@ router.use(isAuthenticated);
 router.post("/", isAuthenticated, isAdmin, createCustomPackage);
 router.get("/", isAuthenticated, isAdmin, getAllCustomPackages);
 router.get("/:packageId", isAuthenticated, isAdmin, getCustomPackageById);
+router.get("/my-packages", isAuthenticated, getAllCustomPackagesByCustomer);
 router.patch("/:packageId", isAuthenticated, isAdmin, updateCustomPackage);
 router.delete("/:packageId", isAuthenticated, isAdmin, deleteCustomPackage);
 
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
-  createOrder,
-  getOrders,
-  getOrder,
-  updateOrder,
-  deleteOrder,
+    createOrder,
+    getOrders,
+    getOrder,
+    updateOrder,
+    deleteOrder,
+    createClaimOnOrder,
 } from "../controllers/orders.controller.js";
 import { isAuthenticated } from "../middlewares/authentication.middleware.js";
 
@@ -15,5 +16,6 @@ router.get("/", isAuthenticated, getOrders);
 router.get("/:orderId", isAuthenticated, getOrder);
 router.patch("/:orderId", isAuthenticated, updateOrder);
 router.delete("/:orderId", isAuthenticated, deleteOrder);
+router.post("/create-claim/:orderId", isAuthenticated, createClaimOnOrder);
 
 export default router;
