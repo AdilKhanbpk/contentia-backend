45c1614a9557d3e8a7d975117d2255f7c7fc130d
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-25 15:23:06 +0500

    developed applied creators controller for an order

--- routes/admin/adminOrder.routes.js
+++ routes/admin/adminOrder.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {
-  isAuthenticated,
-  isAdmin,
+    isAuthenticated,
+    isAdmin,
 } from "../../middlewares/authentication.middleware.js";
 import {
-  createOrder,
-  deleteOrder,
-  getOrderById,
-  getOrders,
-  updateOrder,
+    createOrder,
+    deleteOrder,
+    getAppliedCreatorsOnOrders,
+    getOrderById,
+    getOrders,
+    updateOrder,
 } from "../../controllers/admin/adminOrder.controller.js";
 
 const router = express.Router();
@@ -18,5 +19,11 @@ router.get("/", isAuthenticated, isAdmin, getOrders);
 router.get("/:orderId", isAuthenticated, isAdmin, getOrderById);
 router.patch("/:orderId", isAuthenticated, isAdmin, updateOrder);
 router.delete("/:orderId", isAuthenticated, isAdmin, deleteOrder);
+router.get(
+    "/applied-creators/:orderId",
+    isAuthenticated,
+    isAdmin,
+    getAppliedCreatorsOnOrders
+);
 
 export default router;
