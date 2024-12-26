3a2dc9822f7a26b316cfd823f088e8e0e44968c1
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-26 11:32:27 +0500

    modified the get all applied creators

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -8,6 +8,7 @@ import {
     createClaimOnOrder,
 } from "../controllers/orders.controller.js";
 import { isAuthenticated } from "../middlewares/authentication.middleware.js";
+import { applyForOrder } from "../controllers/creator.controller.js";
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.get("/:orderId", isAuthenticated, getOrder);
 router.patch("/:orderId", isAuthenticated, updateOrder);
 router.delete("/:orderId", isAuthenticated, deleteOrder);
 router.post("/create-claim/:orderId", isAuthenticated, createClaimOnOrder);
+router.post("/apply-for-order/:orderId", isAuthenticated, applyForOrder);
 
 export default router;
