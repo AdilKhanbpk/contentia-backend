6ea534bbfab57b74cca117e0aa880747dab341b7
saudkhanbpk <saudkhanbpk@gmail.com>
2025-02-13 12:39:09 +0500

    No code changes made; skipping commit.

--- helpers/notificationTemplates.js
+++ helpers/notificationTemplates.js
@@ -1,3 +1,238 @@
+// export const notificationTemplates = {
+//     generalNotification: ({
+//         adminName,
+//         title,
+//         details,
+//         userType = "all",
+//         eventType = "general",
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title,
+//             details: `This is a general notification from admin ${adminName} with the following details: ${details}`,
+//             userType,
+//             eventType,
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     creatorRegistration: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "New Creator Registration",
+//             details: `A new creator has successfully registered. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "admin",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+
+//     creatorApplyForOrder: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Creator Application for Order",
+//             details: `A creator has applied for an order. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "admin",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+
+//     creatorApprovalForOrderByAdmin: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Order Approval Notification",
+//             details: `The following creator has been approved for an order:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "creator",
+//             eventType: "creator",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+
+//     customerNotificationForOrderAssignedToCreator: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Order Assigned Notification",
+//             details: `The following creator has been assigned to your order:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "customer",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+
+//     creatorRejectionForOrder: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Order Rejection Notification",
+//             details: `The following creator has been rejected for an order:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "creator",
+//             eventType: "creator",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     creatorRegistrationByAdmin: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Admin-Initiated Creator Registration",
+//             details: `An admin has registered a new creator. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "creator",
+//             eventType: "creator",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     creatorApproval: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Creator Approval Notification",
+//             details: `The following creator has been approved by an admin:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "creator",
+//             eventType: "creator",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     creatorRejection: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Creator Rejection Notification",
+//             details: `The following creator has been rejected by an admin:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "creator",
+//             eventType: "creator",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     orderCreationByCustomer: ({
+//         customerName,
+//         customerEmail,
+//         customerPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "New Order Created",
+//             details: `A customer has created a new order. Details:\nName: ${customerName}\nEmail: ${customerEmail}\nPhone Number: ${customerPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "admin",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     orderCreationByAdminForCustomer: ({
+//         customerName,
+//         customerEmail,
+//         customerPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Order Created by Admin",
+//             details: `An admin has created a new order on behalf of a customer. Details:\nCustomer Name: ${customerName}\nCustomer Email: ${customerEmail}\nCustomer Phone Number: ${customerPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "admin",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+
+//     reportAnOrderFromCustomer: ({
+//         customerName,
+//         customerEmail,
+//         customerPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Order Issue Reported",
+//             details: `A customer has reported an issue with an order. Details:\nName: ${customerName}\nEmail: ${customerEmail}\nPhone Number: ${customerPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "admin",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+//     reportAnOrderFromAdmin: ({
+//         creatorName,
+//         creatorEmail,
+//         creatorPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "Order Issue Reported by Admin",
+//             details: `An admin has reported an issue with an order. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+//             userType: "creator",
+//             eventType: "creator",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+
+//     packageCreationByAdmin: ({
+//         customerName,
+//         customerEmail,
+//         customerPhoneNumber,
+//         targetUsers = [],
+//         metadata = {},
+//     }) => {
+//         return {
+//             title: "New Package Created",
+//             details: `A new package has been created by an admin. Customer Details:\nName: ${customerName}\nEmail: ${customerEmail}\nPhone Number: ${customerPhoneNumber}`,
+//             userType: "customer",
+//             eventType: "admin",
+//             users: targetUsers,
+//             metadata,
+//         };
+//     },
+// };
+
 export const notificationTemplates = {
     generalNotification: ({
         adminName,
@@ -10,7 +245,7 @@ export const notificationTemplates = {
     }) => {
         return {
             title,
-            details: `This is a general notification from admin ${adminName} with the following details: ${details}`,
+            details: `Admin ${adminName} has sent a general notification with the following message: ${details}`,
             userType,
             eventType,
             users: targetUsers,
@@ -26,7 +261,7 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "New Creator Registration",
-            details: `A new creator has successfully registered. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+            details: `A new creator named ${creatorName} has successfully registered with the email ${creatorEmail} and phone number ${creatorPhoneNumber}.`,
             userType: "customer",
             eventType: "admin",
             users: targetUsers,
@@ -43,7 +278,7 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "Creator Application for Order",
-            details: `A creator has applied for an order. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+            details: `Creator ${creatorName} has applied for an order using the email ${creatorEmail} and phone number ${creatorPhoneNumber}.`,
             userType: "customer",
             eventType: "admin",
             users: targetUsers,
@@ -60,7 +295,7 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "Order Approval Notification",
-            details: `The following creator has been approved for an order:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+            details: `The creator ${creatorName} has been approved for an order. Their contact details are email: ${creatorEmail} and phone number: ${creatorPhoneNumber}.`,
             userType: "creator",
             eventType: "creator",
             users: targetUsers,
@@ -77,7 +312,7 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "Order Assigned Notification",
-            details: `The following creator has been assigned to your order:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+            details: `Your order has been assigned to creator ${creatorName}, who can be reached via email at ${creatorEmail} and phone number ${creatorPhoneNumber}.`,
             userType: "customer",
             eventType: "customer",
             users: targetUsers,
@@ -94,61 +329,14 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "Order Rejection Notification",
-            details: `The following creator has been rejected for an order:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
+            details: `The creator ${creatorName} has been rejected for the order. Their email is ${creatorEmail}, and their phone number is ${creatorPhoneNumber}.`,
             userType: "creator",
             eventType: "creator",
             users: targetUsers,
             metadata,
         };
     },
-    creatorRegistrationByAdmin: ({
-        creatorName,
-        creatorEmail,
-        creatorPhoneNumber,
-        targetUsers = [],
-        metadata = {},
-    }) => {
-        return {
-            title: "Admin-Initiated Creator Registration",
-            details: `An admin has registered a new creator. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
-            userType: "creator",
-            eventType: "creator",
-            users: targetUsers,
-            metadata,
-        };
-    },
-    creatorApproval: ({
-        creatorName,
-        creatorEmail,
-        creatorPhoneNumber,
-        targetUsers = [],
-        metadata = {},
-    }) => {
-        return {
-            title: "Creator Approval Notification",
-            details: `The following creator has been approved by an admin:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
-            userType: "creator",
-            eventType: "creator",
-            users: targetUsers,
-            metadata,
-        };
-    },
-    creatorRejection: ({
-        creatorName,
-        creatorEmail,
-        creatorPhoneNumber,
-        targetUsers = [],
-        metadata = {},
-    }) => {
-        return {
-            title: "Creator Rejection Notification",
-            details: `The following creator has been rejected by an admin:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
-            userType: "creator",
-            eventType: "creator",
-            users: targetUsers,
-            metadata,
-        };
-    },
+
     orderCreationByCustomer: ({
         customerName,
         customerEmail,
@@ -158,13 +346,14 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "New Order Created",
-            details: `A customer has created a new order. Details:\nName: ${customerName}\nEmail: ${customerEmail}\nPhone Number: ${customerPhoneNumber}`,
+            details: `A new order has been placed by customer ${customerName}, who can be contacted at ${customerEmail} or phone number ${customerPhoneNumber}.`,
             userType: "customer",
             eventType: "admin",
             users: targetUsers,
             metadata,
         };
     },
+
     orderCreationByAdminForCustomer: ({
         customerName,
         customerEmail,
@@ -174,57 +363,7 @@ export const notificationTemplates = {
     }) => {
         return {
             title: "Order Created by Admin",
-            details: `An admin has created a new order on behalf of a customer. Details:\nCustomer Name: ${customerName}\nCustomer Email: ${customerEmail}\nCustomer Phone Number: ${customerPhoneNumber}`,
-            userType: "customer",
-            eventType: "admin",
-            users: targetUsers,
-            metadata,
-        };
-    },
-
-    reportAnOrderFromCustomer: ({
-        customerName,
-        customerEmail,
-        customerPhoneNumber,
-        targetUsers = [],
-        metadata = {},
-    }) => {
-        return {
-            title: "Order Issue Reported",
-            details: `A customer has reported an issue with an order. Details:\nName: ${customerName}\nEmail: ${customerEmail}\nPhone Number: ${customerPhoneNumber}`,
-            userType: "customer",
-            eventType: "admin",
-            users: targetUsers,
-            metadata,
-        };
-    },
-    reportAnOrderFromAdmin: ({
-        creatorName,
-        creatorEmail,
-        creatorPhoneNumber,
-        targetUsers = [],
-        metadata = {},
-    }) => {
-        return {
-            title: "Order Issue Reported by Admin",
-            details: `An admin has reported an issue with an order. Details:\nName: ${creatorName}\nEmail: ${creatorEmail}\nPhone Number: ${creatorPhoneNumber}`,
-            userType: "creator",
-            eventType: "creator",
-            users: targetUsers,
-            metadata,
-        };
-    },
-
-    packageCreationByAdmin: ({
-        customerName,
-        customerEmail,
-        customerPhoneNumber,
-        targetUsers = [],
-        metadata = {},
-    }) => {
-        return {
-            title: "New Package Created",
-            details: `A new package has been created by an admin. Customer Details:\nName: ${customerName}\nEmail: ${customerEmail}\nPhone Number: ${customerPhoneNumber}`,
+            details: `An admin has placed an order on behalf of customer ${customerName}. The customer's email is ${customerEmail}, and their phone number is ${customerPhoneNumber}.`,
             userType: "customer",
             eventType: "admin",
             users: targetUsers,
