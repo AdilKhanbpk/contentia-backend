d4a445bba4e1fc456c072b0867241bb69ad696a9
saudkhanbpk <saudkhanbpk@gmail.com>
2025-02-20 16:33:10 +0500

    Add deleteFileFromCloudinary utility for file deletion from Cloudinary

--- controllers/creator.controller.js
+++ controllers/creator.controller.js
@@ -14,6 +14,7 @@ import {
     listSpecificFolderInParentFolder,
 } from "../utils/googleDrive.js";
 import {
+    deleteFileFromCloudinary,
     uploadFileToCloudinary,
     uploadMultipleFilesToCloudinary,
 } from "../utils/Cloudinary.js";
